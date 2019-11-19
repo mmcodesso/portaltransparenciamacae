003eb7d23720fddbe835df11d525ec20a61356d5
Mauricio <mmcodesso@users.noreply.github.com>
2019-11-19 17:28:32 -0300

    Add more features

--- 02-Portal_Transparencia.py
+++ 02-Portal_Transparencia.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -8,8 +9,9 @@ url_home = "http://sistemas.macae.rj.gov.br/transparencia/index.asp?acao=3&item=
 def set_initial_page(year,initial_date,final_date,driver):
 
     #Set year field
-    cmbAno_field = driver.find_element_by_id('cmbAno')
-    cmbAno_field.send_keys(year)
+    cmbAno_field = Select(driver.find_element_by_id('cmbAno'))
+    cmbAno_field.select_by_visible_text(year)
+
 
     # Set  Initial_date
     initial_date_field = driver.find_element_by_id('txtDataInicial')
@@ -26,6 +28,17 @@ def set_initial_page(year,initial_date,final_date,driver):
     gerar_buton.click()
     return
 
+def download_tabela_empenho(driver):
+
+    page = BeautifulSoup(driver.page_source, 'lxml')
+    table = page.find('table', id='tbTabela')
+
+    tabela_empenho_pagina_df = pd.read_html(str(table),skiprows=1)
+    tabela_empenho_pagina_df = tabela_empenho_pagina_df[0]
+    tabela_empenho_pagina_df.to_csv('credores.csv',mode='a')
+
+    return
+
 
 def main(url):
     options = webdriver.ChromeOptions()
@@ -40,14 +53,18 @@ def main(url):
     #Set initial Page information
     set_initial_page(year,inicial_date,final_date,driver)
 
+    #Save Credores Table
+    download_tabela_empenho(driver)
+
 
     #Close Chrome
-
     driver.close()
+
+    return
 
 
 if __name__ == "__main__":
-    #explicit_wait = 3
+    explicit_wait = 3
     year = "2015"
     inicial_date = '01/01/2015'
     final_date = '31/12/2015'
