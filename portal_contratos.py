8853e3fff1d34c06bfe22eda8d61cde8aa9db64e
errearanhas <errearanhas@gmail.com>
2019-12-27 18:51:26 -0300

    add link_id em portal_contratos.py para facilitar raspagem via URL

--- portal_contratos.py
+++ portal_contratos.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
 
-def get_number_of_pages():
+def get_number_of_pages(driver):
+    expand = Select(driver.find_element_by_name('tb-result_length'))
+    expand.select_by_visible_text(str(100))
     html_list = driver.find_element_by_xpath('//*[@id="tb-result_paginate"]/div/ul')
     last = html_list.find_elements_by_tag_name("li")[-2].text
     last = int(last)
     return last
 
 
-def goto_next_page():
+def goto_next_page(driver):
     try:
         elem = driver.find_element_by_link_text('Próximo')
         elem.click()
@@ -20,20 +23,28 @@ def goto_next_page():
     return
 
 
-def get_one_table():
+def get_one_table(driver):
+    lista_id = []
     page = BeautifulSoup(driver.page_source, 'lxml')
-    element = page.find(id='tb-result_wrapper')
+    element = page.find(id='tb-result')
     table = pd.read_html(str(element))[0]
+
+    temp = element.find_all('tr')
+    for i in range(len(temp)):
+        if i>0:
+            id = temp[i]['id']
+            lista_id.append(id)
+    table['link_id'] = lista_id
     return table
 
 
-def get_full_table():
+def get_full_table(driver):
     full_table = pd.DataFrame()
-    last = get_number_of_pages()
+    last = get_number_of_pages(driver)
     for i in range(last):
-        one_table = get_one_table()
+        one_table = get_one_table(driver)
         full_table = full_table.append(one_table)
-        goto_next_page()
+        goto_next_page(driver)
         time.sleep(1)
     return full_table
 
@@ -50,12 +61,15 @@ def main():
     driver.implicitly_wait(10)  # seconds
 
     # Open Chrome
-    driver.get(url_contratos)
-    button = driver.find_element_by_id('btn-buscar')
+    time.sleep(5)
+    driver.get(url)
+    time.sleep(5)
+    button = driver.find_element_by_xpath('//*[@id="btn-buscar"]')
     button.click()
-    full_table = get_full_table()
+    full_table = get_full_table(driver)
     print('Captura finalizada ---- ' + time.ctime(time.time()))
     full_table.to_csv('full_table_contratos.csv', index=0)
+
     return
 
 
