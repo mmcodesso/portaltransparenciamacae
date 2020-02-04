f81195db467e359938e2319ef0d21b74f7d2276f
Renato Aranha <renatoaranha@renatos-mbp.br.ibm.com>
2020-02-04 16:31:11 -0300

    adding new version of 01_portalTransparencia.py to get liquidacoes and pagamentos; renaming scripts

---
+++ 01_PortalTransparencia_V2.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import time
+import sys
+
+
+def set_initial_page(ano, dt_inicio, dt_fim, drvr):
+
+    # Set ano field
+    cmb_ano_field = Select(drvr.find_element_by_id('cmbAno'))
+    cmb_ano_field.select_by_visible_text(ano)
+
+    # Set Initial_date
+    initial_date_field = drvr.find_element_by_id('txtDataInicial')
+    initial_date_field.clear()
+    initial_date_field.send_keys(dt_inicio)
+
+    # set final_date
+    initial_date_field = drvr.find_element_by_id('txtDataFinal')
+    initial_date_field.clear()
+    initial_date_field.send_keys(dt_fim)
+
+    # confirm
+    gerar_button = drvr.find_element_by_id('confirma')
+    gerar_button.click()
+    return
+
+
+def goto_companies_documents(drvr, company_name):
+    link = drvr.find_element_by_link_text(company_name)
+    link.click()
+    return
+
+
+def download_tabela_empenho(driver, ano, pag_atual=True):
+    page = BeautifulSoup(driver.page_source, 'lxml')
+    table = page.find('table', id='tbTabela')
+
+    tabela_credores_site = pd.read_html(str(table), header=1)[0]
+    tabela_credores_site['ano'] = ano
+    tabela_credores_site['download_status'] = 0
+    tabela_credores_site['download_status_liquida'] = 0
+    tabela_credores_site.download_status_liquida = \
+        tabela_credores_site.download_status_liquida.mask(tabela_credores_site['Valor Liquidado'] == 'R$ 0,00', other=2)
+
+    try:
+        tabela_credores = pd.read_csv('credores_liquidacoes_' + str(ano) + '.csv')
+        tabela_credores_site = tabela_credores_site[~tabela_credores_site.Nome.isin(tabela_credores.Nome)]
+        tabela_credores_site = tabela_credores.append(tabela_credores_site, sort=True)
+        tabela_credores_site.to_csv('credores_liquidacoes_' + str(ano) + '.csv', index=0, sep="\t")
+    except Exception:
+        tabela_credores_site.to_csv('credores_liquidacoes_' + str(ano) + '.csv', index=0, sep="\t")
+
+    credores_pagina = tabela_credores_site[tabela_credores_site.download_status_liquida == 0].Nome
+
+    if pag_atual:
+        credores = tabela_credores_site[(tabela_credores_site.Nome.isin(credores_pagina))
+                                        & (tabela_credores_site.download_status_liquida == 0)].Nome
+    else:
+        credores = tabela_credores_site[(tabela_credores_site.download_status_liquida == 0)].Nome
+
+    for i, credor in enumerate(credores):
+        try:
+            goto_companies_documents(driver, credor)
+            print('----> credor atual: ' + credor, end="\r")
+            page_empenhos = BeautifulSoup(driver.page_source, 'lxml')
+        except Exception:
+            continue
+
+        try:
+            table_empenhos = page_empenhos.find('table', id='tbTabela1')
+            empenho_df = pd.read_html(str(table_empenhos), header=1, skiprows=1, converters={'Número do Empenho': str})
+        except Exception:
+            try:
+                table_empenhos = page_empenhos.find('table', id='tbTabela2')
+                empenho_df = pd.read_html(str(table_empenhos), header=1, converters={'Número do Empenho': str})
+            except Exception:
+                tabela_credores_site['download_status_liquida'] = np.where((tabela_credores_site.Nome == credor), 2,
+                                                                           tabela_credores_site.download_status)
+                tabela_credores_site.to_csv('credores_liquidacoes_' + str(ano) + '.csv', index=0, sep="\t")
+                driver.find_element_by_xpath('//*[@id="tbAtualizacao"]/tbody/tr[2]/td/input[1]').click()
+                continue
+
+        empenho_df = empenho_df[0]
+        empenho_df = empenho_df[:-1]  # remove a ultima linha com totais
+        empenho_df = empenho_df[['Data Emissão Empenho', 'Número do Empenho', 'Unidade Gestora',
+                                 'Credor', 'Valor Empenhado', 'Valor Em Liquidação', 'Valor Liquidado',
+                                 'Valor Pago', 'Valor Anulado']]
+
+        numeros_empenhos = empenho_df['Número do Empenho']
+
+        table_detalhe_liquida = pd.DataFrame()
+        table_detalhe_pag = pd.DataFrame()
+
+        if numeros_empenhos.any():
+            for empenho in numeros_empenhos:
+                goto_companies_documents(driver, empenho)
+                page_detalhe_empenho = BeautifulSoup(driver.page_source, 'lxml')
+                relevant_tables = page_detalhe_empenho.find_all('table', id='tbTabela')
+                for j in relevant_tables:
+                    if j.find('div', attrs={'style': "text-align:center;"}).text == 'Liquidações':
+                        table_temp_liquida = pd.read_html(str(j), header=1)[0]
+                        table_temp_liquida['empenho'] = empenho
+                        table_temp_liquida['credor'] = credor
+                        table_detalhe_liquida = table_detalhe_liquida.append(table_temp_liquida, sort=True)
+                        del table_temp_liquida
+                    elif j.find('div', attrs={'style': "text-align:center;"}).text == 'Pagamentos':
+                        table_temp_pag = pd.read_html(str(j), header=1)[0]
+                        table_temp_pag['empenho'] = empenho
+                        table_temp_pag['credor'] = credor
+                        table_detalhe_pag = table_detalhe_pag.append(table_temp_pag, sort=True)
+                        del table_temp_pag
+
+                driver.find_element_by_xpath('//*[@id="tbAtualizacao"]/tbody/tr[2]/td/input[1]').click()
+
+            driver.find_element_by_xpath('//*[@id="tbAtualizacao"]/tbody/tr[2]/td/input[1]').click()
+
+        try:
+            df_liq = pd.read_csv('credores_liquidacoes_' + str(ano) + '.csv')
+            tabela_detalhe_liquida = df_liq.append(tabela_detalhe_liquida, sort=True)
+            tabela_detalhe_liquida = tabela_detalhe_liquida.drop_duplicates(keep='first')
+            tabela_detalhe_liquida.to_csv('credores_liquidacoes_' + str(ano) + '.csv', index=0, sep="\t")
+
+            df_pag = pd.read_csv('credores_pagamentos_' + str(ano) + '.csv')
+            table_detalhe_pag = df_pag.append(table_detalhe_pag, sort=True)
+            table_detalhe_pag = table_detalhe_pag.drop_duplicates(keep='first')
+            table_detalhe_pag.to_csv('credores_pagamentos_' + str(ano) + '.csv', index=0, sep="\t")
+        except:
+            table_detalhe_liquida.to_csv('credores_pagamentos_' + str(ano) + '.csv', index=0, sep="\t")
+            table_detalhe_pag.to_csv('credores_pagamentos_' + str(ano) + '.csv', index=0, sep="\t")
+
+        tabela_credores_site['download_status_liquida'] = np.where((tabela_credores_site.Nome == credor), 1,
+                                                                   tabela_credores_site.download_status_liquida)
+
+        tabela_credores_site.to_csv('credores_liquidacoes_' + str(ano) + '.csv', index=0, sep="\t")
+
+    return
+
+
+def check_exists_next_page(drvr):
+    try:
+        drvr.find_element_by_link_text('Próxima página')
+    except Exception:
+        return False
+    return True
+
+
+def get_current_page(drvr):
+    try:
+        elem = drvr.find_element_by_id('txtPaginacao')
+        curr_page = elem.get_attribute('innerHTML')
+        return curr_page
+    except Exception:
+        print('Não foi possível obter a página atual.')
+    return
+
+
+def main():
+    print('Processo Iniciado ---- ' + time.ctime(time.time()))
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=options)  # run if chromedriver is in PATH
+
+    # driver = webdriver.Chrome(
+    #    options=options,
+    #    # executable_path='/home/rsa/PycharmProjects/ebape/portaltransparenciamacae/chromedriver')
+    #    executable_path='/portaltransparenciamacae/chromedriver')
+
+    # Open Chrome and set initial page information
+    driver.get(url)
+    set_initial_page(year, initial_date, final_date, driver)
+
+    while check_exists_next_page(driver):
+        download_tabela_empenho(driver, year)
+        goto_companies_documents(driver, 'Próxima página')
+        print(get_current_page(driver) + ' ---- ' + time.ctime(time.time()))
+    else:
+        download_tabela_empenho(driver, year)
+        print('Não existem mais empenhos ---- ' + time.ctime(time.time()))
+        # Close Chrome
+        driver.close()
+
+    return
+
+if __name__ == "__main__":
+    try:
+        year = sys.argv[1]
+        initial_date = '01/01/'+str(year)
+        final_date = '31/12/'+str(year)
+        url_home = "http://sistemas.macae.rj.gov.br/transparencia/index.asp?acao=3&item=10"
+        url = url_home
+        main()
+    except:
+        print('\nInformar ano desejado.\n')
--- temp.py
+++ temp.py
@@ -1,3 +1,62 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
 
-df1 = pd.read_csv('./portaltransparenciamacae/detalhes_emp_2015.csv')
+
+def set_initial_page(ano, dt_inicio, dt_fim, drvr):
+
+    # Set ano field
+    cmb_ano_field = Select(drvr.find_element_by_id('cmbAno'))
+    cmb_ano_field.select_by_visible_text(ano)
+
+    # Set Initial_date
+    initial_date_field = drvr.find_element_by_id('txtDataInicial')
+    initial_date_field.clear()
+    initial_date_field.send_keys(dt_inicio)
+
+    # set final_date
+    initial_date_field = drvr.find_element_by_id('txtDataFinal')
+    initial_date_field.clear()
+    initial_date_field.send_keys(dt_fim)
+
+    # confirm
+    gerar_button = drvr.find_element_by_id('confirma')
+    gerar_button.click()
+    return
+
+def main():
+    print('Processo Iniciado ---- ' + time.ctime(time.time()))
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+
+    driver = webdriver.Chrome(
+        options=options,
+        executable_path='/home/rsa/PycharmProjects/ebape/portaltransparenciamacae/chromedriver')
+
+    # Open Chrome and set initial page information
+    driver.get(url)
+    set_initial_page(year, initial_date, final_date, driver)
+
+    while check_exists_next_page(driver):
+        download_tabela_empenho(driver, year)
+        goto_companies_documents(driver, 'Próxima página')
+        print(get_current_page(driver) + ' ---- ' + time.ctime(time.time()))
+    else:
+        download_tabela_empenho(driver, year)
+        print('Não existem mais empenhos ---- ' + time.ctime(time.time()))
+        # Close Chrome
+        driver.close()
+
+    return
+
+
+if __name__ == "__main__":
+    try:
+        year = sys.argv[1]
+        initial_date = '01/01/'+str(year)
+        final_date = '31/12/'+str(year)
+        url_home = "http://sistemas.macae.rj.gov.br/transparencia/index.asp?acao=3&item=10"
+        url = url_home
+        main()
+    except:
+        print('\nInformar ano desejado.\n')
