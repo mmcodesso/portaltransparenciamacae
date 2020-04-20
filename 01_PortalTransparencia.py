52f2e17a338a479fa74a0a9f3822bfe7a095137e
Renato Aranha <errearanhas@gmail.com>
2020-04-19 22:36:11 -0300

    cleaning folders

--- 01_PortalTransparencia.py
+++ 01_PortalTransparencia.py
@@ -154,7 +154,7 @@ def main():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_experimental_option("detach", True)
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
     # driver = webdriver.Chrome(options=options)  # run if chromedriver is in PATH
     # driver = webdriver.Chrome(options=options, executable_path='/Users/renatoaranha/PycharmProjects/webdriver')
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
