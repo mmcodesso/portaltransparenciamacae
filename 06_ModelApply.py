85a63569c6e8e70069369b1404fb9868c4fe1041
Renato Aranha <errearanhas@gmail.com>
2020-06-15 03:26:02 -0300

    adding query gustavo explicitly

--- 06_ModelApply.py
+++ 06_ModelApply.py
@@ -8,10 +8,26 @@ seed(1)
 
 # df = pd.read_csv('merged_times.csv', engine='python')  # load data set
 df = pd.read_csv('merged_times2.csv', engine='python')  # load data set
+df = df[['empenho', 'credor', 'parte_relac', 'tempo_entre_empenho_liquidacao',
+         'tempo_entre_liquidacao_pagamento', 'tempo_entre_empenho_pagamento',
+         'tempo_entre_homologacao_pagamento']].drop_duplicates()
 
 grp = df.groupby('parte_relac')[['tempo_entre_empenho_liquidacao',
                                  'tempo_entre_liquidacao_pagamento', 'tempo_entre_empenho_pagamento',
                                  'tempo_entre_homologacao_pagamento']].agg([np.mean, np.median, np.max, np.min]).T
+
+grp = round(grp, 2)
+
+perform_anova(df, 'tempo_entre_empenho_liquidacao')
+
+d = pd.DataFrame({
+    'parte_relac 0': df[df.parte_relac == 0]['tempo_entre_empenho_liquidacao'],
+    'parte_relac 1': df[df.parte_relac == 1]['tempo_entre_empenho_liquidacao'],
+})
+ax = d.plot.kde()
+ax.set_title("tempo_entre_empenho_liquidacao")
+ax.set_xlim(-100, 500)
+
 
 # Analysis of Variance test
 # references:
@@ -19,29 +35,23 @@ grp = df.groupby('parte_relac')[['tempo_entre_empenho_liquidacao',
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.f_oneway.html
 # https://www.statisticshowto.com/probability-and-statistics/f-statistic-value-test/
 
-# generate three independent samples
-data1 = df[df.parte_relac == 0]['tempo_entre_empenho_liquidacao']
-data2 = df[df.parte_relac == 1]['tempo_entre_empenho_liquidacao']
-data3 = df[df.parte_relac == 0]['tempo_entre_liquidacao_pagamento']
-data4 = df[df.parte_relac == 1]['tempo_entre_liquidacao_pagamento']
-data5 = df[df.parte_relac == 0]['tempo_entre_empenho_pagamento']
-data6 = df[df.parte_relac == 1]['tempo_entre_empenho_pagamento']
-data7 = df[df.parte_relac == 0]['tempo_entre_homologacao_pagamento']
-data8 = df[df.parte_relac == 1]['tempo_entre_homologacao_pagamento']
 
-first = data7
-sec = data8
+def perform_anova(df, coluna):
+    col = str(coluna)
+    data1 = df[df.parte_relac == 0][col]
+    data2 = df[df.parte_relac == 1][col]
 
-# summarize
-print('tempo_entre_liquidacao_pagamento\n')
-print('parte_relac = 0: mean=%.2f stdv=%.2f' % (np.mean(first), np.std(first)))
-print('parte_relac = 1: mean=%.2f stdv=%.2f\n' % (np.mean(sec), np.std(sec)))
-# compare samples
-stat, p = f_oneway(first, sec)
-print('F Stat=%.3f, p=%.5f' % (stat, p))
-# interpret
-alpha = 0.05
-if p > alpha:
-    print('Same distributions (fail to reject H0)')
-else:
-    print('Different distributions (reject H0)')
+    # summarize
+    print(coluna + '\n')
+    print('parte_relac = 0: mean=%.2f stdv=%.2f' % (np.mean(data1), np.std(data1)))
+    print('parte_relac = 1: mean=%.2f stdv=%.2f\n' % (np.mean(data2), np.std(data2)))
+    # compare samples
+    stat, p = f_oneway(data1, data2)
+    print('F Stat=%.3f, p=%.5f' % (stat, p))
+    # interpret
+    alpha = 0.05
+    if p > alpha:
+        print('Same distributions (fail to reject H0)')
+    else:
+        print('Different distributions (reject H0)')
+    return
