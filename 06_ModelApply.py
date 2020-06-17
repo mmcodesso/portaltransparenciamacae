e8008907d8870992b49b58255441880e9d4be074
Renato Aranha <errearanhas@gmail.com>
2020-06-17 01:47:05 -0300

    adding statistical tests

--- 06_ModelApply.py
+++ 06_ModelApply.py
@@ -1,66 +1,145 @@
 import pandas as pd
 import numpy as np
-from numpy.random import seed
 from scipy.stats import f_oneway
+from scipy.stats import ttest_ind
+import sqlite3
+from statsmodels.stats.weightstats import ztest
 
-# seed the random number generator
-seed(1)
+# df = pd.read_csv('merged_times51373.csv')  # load data set
+# df = pd.read_csv('merged_times2.csv')  # load data set
 
-df = pd.read_csv('merged_times51373.csv', engine='python')  # load data set
-a=df[df.columns.difference(['nome',
-                 'soma_de_percentual_de_doacao',
-                 'ano_eleicao',
-                 'eleicao',
-                 'sigla_do_partido',
-                 'soma_de_percentual_de_despesas',
-                 'secretaria/_orgao'])].drop_duplicates()
-a.to_sql('merged_times', con=conn, if_exists='replace', index=False)
-
-df = pd.read_csv('merged_times2.csv', engine='python')  # load data set
-df = df[['empenho', 'credor', 'parte_relac', 'tempo_entre_empenho_liquidacao',
-         'tempo_entre_liquidacao_pagamento', 'tempo_entre_empenho_pagamento',
-         'tempo_entre_homologacao_pagamento']].drop_duplicates()
-
-grp = df.groupby('parte_relac')[['tempo_entre_empenho_liquidacao',
-                                 'tempo_entre_liquidacao_pagamento', 'tempo_entre_empenho_pagamento',
-                                 'tempo_entre_homologacao_pagamento']].agg([np.mean, np.median, np.max, np.min]).T
-
-grp = round(grp, 2)
-
-perform_anova(df, 'tempo_entre_empenho_liquidacao')
-
-d = pd.DataFrame({
-    'parte_relac 0': df[df.parte_relac == 0]['tempo_entre_empenho_liquidacao'],
-    'parte_relac 1': df[df.parte_relac == 1]['tempo_entre_empenho_liquidacao'],
-})
-ax = d.plot.kde()
-ax.set_title("tempo_entre_empenho_liquidacao")
-ax.set_xlim(-100, 500)
+def run_query(query):
+    database = r"database_macae.db"
+    conn = sqlite3.connect(database)
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    col_name_list = [i[0] for i in cur.description]
+    df = pd.DataFrame(rows, columns=col_name_list)
+    return df
 
 
-# Analysis of Variance test
-# references:
-# https://machinelearningmastery.com/parametric-statistical-significance-tests-in-python/
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.f_oneway.html
-# https://www.statisticshowto.com/probability-and-statistics/f-statistic-value-test/
+def descriptive_stats(df):
+    grp = df.groupby('parte_relac')[['tempo_entre_liquidacao_pagamento']].agg(
+        [np.size, np.mean, np.median, np.std, max, min]).T
+
+    grp = round(grp, 2)
+    print(grp)
+    print("\n")
+    return
 
 
 def perform_anova(df, coluna):
+    # Analysis of Variance test
+    # references:
+    # https://machinelearningmastery.com/parametric-statistical-significance-tests-in-python/
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.f_oneway.html
+    # https://www.statisticshowto.com/probability-and-statistics/f-statistic-value-test/
+
     col = str(coluna)
     data1 = df[df.parte_relac == 0][col]
     data2 = df[df.parte_relac == 1][col]
 
     # summarize
-    print(coluna + '\n')
+    print(coluna + ': ANOVA \n')
     print('parte_relac = 0: mean=%.2f stdv=%.2f' % (np.mean(data1), np.std(data1)))
     print('parte_relac = 1: mean=%.2f stdv=%.2f\n' % (np.mean(data2), np.std(data2)))
     # compare samples
     stat, p = f_oneway(data1, data2)
-    print('F Stat=%.3f, p=%.5f' % (stat, p))
-    # interpret
     alpha = 0.05
+    print('F Stat=%.3f, p=%.3f, alpha=%.2f' % (stat, p, alpha))
+    # interpret
     if p > alpha:
         print('Same distributions (fail to reject H0)')
     else:
         print('Different distributions (reject H0)')
+    print("\n")
     return
+
+
+def perform_ttest(df, coluna):
+    col = str(coluna)
+    data1 = df[df.parte_relac == 0][col]
+    data2 = df[df.parte_relac == 1][col]
+
+    # summarize
+    print(coluna + ': T-TEST\n')
+    print('parte_relac = 0: mean=%.2f stdv=%.2f' % (np.mean(data1), np.std(data1)))
+    print('parte_relac = 1: mean=%.2f stdv=%.2f\n' % (np.mean(data2), np.std(data2)))
+    # compare samples
+    stat, p = ttest_ind(data1, data2)
+    alpha = 0.05
+    print('Stat=%.3f, p=%.3f, alpha=%.2f' % (stat, p, alpha))
+    # interpret
+    if p > alpha:
+        print('Same distributions (fail to reject H0)')
+    else:
+        print('Different distributions (reject H0)')
+    print("\n")
+    return
+
+
+def perform_ztest(df, coluna):
+    col = str(coluna)
+    data1 = df[df.parte_relac == 0][col]
+    data2 = df[df.parte_relac == 1][col]
+
+
+    # summarize
+    print(coluna + ': Z-TEST\n')
+    print('parte_relac = 0: mean=%.2f stdv=%.2f' % (np.mean(data1), np.std(data1)))
+    print('parte_relac = 1: mean=%.2f stdv=%.2f\n' % (np.mean(data2), np.std(data2)))
+    # compare samples
+    stat, p = ztest(data1, data2)
+    alpha = 0.05
+    print('Stat=%.3f, p=%.3f, alpha=%.2f' % (stat, p, alpha))
+    # interpret
+    if p > alpha:
+        print('Same distributions (fail to reject H0)')
+    else:
+        print('Different distributions (reject H0)')
+    print("\n")
+    return
+
+
+def plot_distribution(field, xlim=(-100, 100)):
+    field = str(field)
+    d = pd.DataFrame({
+        'parte_relac 0': df[df.parte_relac == 0][field],
+        'parte_relac 1': df[df.parte_relac == 1][field],
+    })
+    ax = d.plot.kde()
+    ax.set_title(field)
+    ax.set_xlim(xlim[0], xlim[1])
+    return
+
+
+
+query = """select
+parte_relac, tipo_pessoa, tempo_entre_liquidacao_pagamento
+from (select
+case when length("cnpj/cpf")>14
+or "cnpj/cpf" = "999.999.999-99"
+and credor not in ("ALDECI SOARES DA SILVA", "ALVIN ANTONIO MOZER E OUTROS VACINADORES", "ALEX MORAES L, DE FIGUEIREDO E OUTROS", "ANDRE LUIZ MARQUES DE AZEVEDO E OUTROS",
+"ANDRE LUIZ RIBEIRO BRAGA E OUTROS", "CARLOS JOSE CARDOSO BARROCO E OUTROS", "DAVID RODRIGUES DA SILVA BEZERRA", "DENISE PROENCA DE FIGUEIREDO",
+"ESP. DE SVEN LENZ INVET.ELVIRA MARIA MONTEIRO LENZ", "ESPOLIO JAEDER CAMPOS MIRANDA E OUTROS", "GEILCE B. GOMES/EDSON C.F.BARRETO", "GEORGIA KARLA B.G.ROCHA E OUTROS",
+"IRIS MARIA PIMENTEL SALLES MAT 4808", "JAINE SANTOS MADALENA", "JONAS ALVES DE BRITO E OUTROS", "JORGE TAVARES SIQUEIRA E OUTROS", "LUCIMAR GRACA PAULA", "MANUEL RODRIGUES QUEIROZ",
+"RICARDO YATES MEDEIROS", "ROSANE CAMPOS DE MIRANDA - USAR COD 4977", "SAMARA FALCAO JARDIM", "SINARA BASTOS TAVARES", "VALERIA F DE S PINHEIRO E OUTROS",
+"VITOR SALEH FONSECA DE MENDONCA E OUTROS", "ARTHUR PECANHA DE AGUIAR", "WASHINGTON ROBERT DA CONCEICAO", "MARCIO DE AZEVEDO FERNANDES - USAR COD 7892",
+"CARLOS MANUEL FIGUEIREDO MACARIO E OUTROS", "JORGE LUIZ DA COSTA FRANCO E OUTROS")
+then "PJ"
+else "PF"
+end as tipo_pessoa,
+* from merged_times2)
+"""
+
+df = run_query(query)
+df = df[df.tipo_pessoa == "PF"]
+
+descriptive_stats(df)
+
+# perform_ztest(df, 'tempo_entre_liquidacao_pagamento')
+perform_anova(df, 'tempo_entre_liquidacao_pagamento')
+perform_ttest(df, 'tempo_entre_liquidacao_pagamento')
+
+# plot_distribution('tempo_entre_liquidacao_pagamento')
